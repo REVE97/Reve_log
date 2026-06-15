508bba0e8cf3f98bb1fb105bc22ac36b15f128b7
REVE97 <zxcv9675@naver.com>
2026-06-15 13:44:23 +0900

    Chore: 초기 세팅

---
+++ revelog/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+  </nav>
+  
+  <RouterView />
+</template>
+
+<script>
+
+</script>
+
+<style>
+  
+</style>
---
+++ revelog/src/components/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Test
+  </div>
+</template>
+
+<script setup>
+import { ref, computed, watch, onMounted } from 'vue';
+
+</script>
+
+<style scoped>
+
+</style>
